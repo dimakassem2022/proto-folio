2e473e483ea30be569364eaca3d12d1cf615f7e4
unknown <dimakassem296@gmail.com>
2022-09-24 02:12:54 +0300

    Sample 1

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import { LogoIcon } from "./LogoIcon";
+import { SocialIcon } from "./SocialIcon";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          {/* <MailchimpForm /> */}
+          {/* <Col size={12} sm={6}>
+            <LogoIcon/> 
+          </Col> */}
+          <Col 
+          // size={12} sm={6} 
+          className="text-center">
+          <SocialIcon />
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
---
+++ src/components/LogoIcon.js
@@ -0,0 +1,12 @@
+
+import chessQueen from '../asserts/icon/chess-queen.svg';
+
+export const LogoIcon = () => {
+    return (
+        <img
+            src={chessQueen}
+            className="d-inline-block align-top"
+            alt="logo"
+        />
+    )
+}
